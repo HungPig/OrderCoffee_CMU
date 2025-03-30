ad9447624a67836d72aee29b7e2eba85d7d19af9
thuchanh1 <ductuan02072005@gmail.com>
2025-03-30 22:28:17 +0700

    tạo Feature MenuUI

--- src/Order/Menu/Main.java
+++ src/Order/Menu/Main.java
@@ -3,13 +3,30 @@ package Order.Menu;
 
 
 import Order.Drawer.MyDrawerBuilder;
+import Order.tabbed.WindowsTabbed;
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.extras.FlatAnimatedLafChange;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.prefs.Preferences;
+import javax.swing.JToggleButton;
 import javax.swing.UIManager;
 import raven.drawer.Drawer;
 import raven.popup.GlassPanePopup;
+
 
 
 /**
@@ -18,72 +35,123 @@ import raven.popup.GlassPanePopup;
  */
 public class Main extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Main
-     */
+    private JToggleButton toggleDarkMode;
+    private FlatSVGIcon darkIcon;
+    private FlatSVGIcon lightIcon;
+    private static final Preferences prefs = Preferences.userRoot().node("DarkModeSettings");
+    
     public Main() {
         GlassPanePopup.install(this);
         MyDrawerBuilder myDrawerBuilder = new MyDrawerBuilder();
         Drawer.getInstance().setDrawerBuilder(myDrawerBuilder);
         initComponents();
-        
+        WindowsTabbed.getInstance().install(this, boddy);
+                jToggleButton1.putClientProperty(FlatClientProperties.STYLE, ""
+        + "arc:999;"
+        + "borderWidth:0;"
+        + "focusWidth:0;"
+        + "innerFocusWidth:0");
+            
+        jToggleButton1.addActionListener(new ActionListener() {
+
+            private final ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
+            private ScheduledFuture<?> scheduledFuture;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (scheduledFuture != null) {
+                    scheduledFuture.cancel(true);
+                }
+                scheduledFuture = scheduled.schedule(() -> {
+                    changeThemes(jToggleButton1.isSelected());
+                }, 500, TimeUnit.MILLISECONDS);
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        darkLightSwitchIcon = new Order.darkmode.DarkLightSwitchIcon();
+        boddy = new javax.swing.JPanel();
+        jToggleButton1 = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Show Drawer");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        javax.swing.GroupLayout boddyLayout = new javax.swing.GroupLayout(boddy);
+        boddy.setLayout(boddyLayout);
+        boddyLayout.setHorizontalGroup(
+            boddyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        boddyLayout.setVerticalGroup(
+            boddyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 483, Short.MAX_VALUE)
+        );
+
+        jToggleButton1.setIcon(darkLightSwitchIcon);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(boddy, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(734, Short.MAX_VALUE))
+                .addComponent(jToggleButton1)
+                .addGap(0, 787, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(486, Short.MAX_VALUE))
+                .addComponent(boddy, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jToggleButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-            Drawer.getInstance().showDrawer();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
+    private void changeThemes(boolean dark) {
+        if (FlatLaf.isLafDark() != dark) {
+            if (!dark) {
+                EventQueue.invokeLater(() -> {
+                    FlatAnimatedLafChange.showSnapshot();
+                    FlatIntelliJLaf.setup();
+                    FlatLaf.updateUI();
+                    FlatAnimatedLafChange.hideSnapshotWithAnimation();
+                });
+            } else {
+                EventQueue.invokeLater(() -> {
+                    FlatAnimatedLafChange.showSnapshot();
+                    FlatDarculaLaf.setup();
+                    FlatLaf.updateUI();
+                    FlatAnimatedLafChange.hideSnapshotWithAnimation();
+                });
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         FlatRobotoFont.install();
         FlatLaf.registerCustomDefaultsSource("raven.themes");
-        UIManager.put("defaultFont",new Font(FlatRobotoFont.FAMILY,Font.PLAIN, 13));
-        FlatMacDarkLaf.setup();
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Main().setVisible(true);
-            }
-        });
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+
+        boolean isDarkMode = prefs.getBoolean("darkMode", false);
+        if (isDarkMode) {
+            FlatMacDarkLaf.setup();
+        } else {
+            FlatLightLaf.setup();
+        }
+
+        java.awt.EventQueue.invokeLater(() -> new Main().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel boddy;
+    private Order.darkmode.DarkLightSwitchIcon darkLightSwitchIcon;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
