ef6e134a19c129b23afa498a3b6bbb5f709e8b9d
thuchanh1 <ductuan02072005@gmail.com>
2025-03-30 22:28:03 +0700

    tạo Feature MenuUI

--- src/Order/Drawer/MyDrawerBuilder.java
+++ src/Order/Drawer/MyDrawerBuilder.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Order.Drawer;
+import Order.form.TestForm;
+import Order.tabbed.WindowsTabbed;
 import raven.drawer.component.SimpleDrawerBuilder;
 import raven.drawer.component.footer.SimpleFooterData;
 import raven.drawer.component.header.SimpleHeaderData;
@@ -18,7 +20,7 @@ public class MyDrawerBuilder extends SimpleDrawerBuilder {
     public SimpleHeaderData getSimpleHeaderData() {
     return new SimpleHeaderData()
             .setIcon(new AvatarIcon(getClass().getResource("/Order/image/Avatar.jpeg"), 60, 60, 999))
-            .setTitle("ViruSs")
+            .setTitle("Đặng Tiến Hoàng (ViruSs)")
             .setDescription("booking@vrstudio.vn");
 }
 
@@ -26,31 +28,34 @@ public class MyDrawerBuilder extends SimpleDrawerBuilder {
     @Override
     public SimpleMenuOption getSimpleMenuOption() {
         String menus[][] = {
-                {"~MAIN~"},
-                {"Dashboard"},
-                {"~WEB APP~"},
-                {"Email", "Inbox", "Read", "Compost"},
-                {"Chat"},
-                {"Calendar"},
-                {"~COMPONENT~"},
-                {"Advanced UI", "Cropper", "Owl Carousel", "Sweet Alert"},
-                {"Forms", "Basic Elements", "Advanced Elements", "SEditors", "Wizard"},
-                {"~OTHER~"},
-                {"Charts", "Apex", "Flot", "Sparkline"},
-                {"Icons", "Feather Icons", "Flag Icons", "Mdi Icons"},
-                {"Special Pages", "Blank page", "Faq", "Invoice", "Profile", "Pricing", "Timeline"},};
+            {"~MAIN~"},
+            {"Dashboard"},
+            {"~WEB APP~"},
+            {"Email", "Inbox", "Read", "Compost"},
+            {"Chat"},
+            {"Calendar"},
+            {"~COMPONENT~"},
+            {"Advanced UI", "Cropper", "Owl Carousel", "Sweet Alert"},
+            {"Forms", "Basic Elements", "Advanced Elements", "SEditors", "Wizard"},
+            {"~OTHER~"},
+            {"Charts", "Apex", "Flot", "Sparkline"},
+            {"Icons", "Feather Icons", "Flag Icons", "Mdi Icons"},
+            {"Special Pages", "Blank page", "Faq", "Invoice", "Profile", "Pricing", "Timeline"},
+            {"Logout"},};
+
         
         String icons[] = {
-                "dashboard.svg",
-                "email.svg",
-                "chat.svg",
-                "calendar.svg",
-                "ui.svg",
-                "forms.svg",
-                "chart.svg",
-                "icon.svg",
-                "page.svg",
-            };
+            "dashboard.svg",
+            "email.svg",
+            "chat.svg",
+            "calendar.svg",
+            "ui.svg",
+            "forms.svg",
+            "chart.svg",
+            "icon.svg",
+            "page.svg",
+            "logout.svg",}; 
+
         return new SimpleMenuOption()
                 .setMenus(menus)
                 .setIcons(icons)
@@ -59,17 +64,20 @@ public class MyDrawerBuilder extends SimpleDrawerBuilder {
                 .addMenuEvent(new MenuEvent() {
             @Override
             public void selected(MenuAction action, int index, int subIndex) {
+                if (index == 0) {
+                            WindowsTabbed.getInstance().addTab("Test Form", new TestForm());
+                        }
                 System.out.println("Menu selected "+index+" "+subIndex);
             }
                 })
                 .setMenuValidation(new MenuValidation(){
             @Override
             public boolean menuValidation(int index, int subIndex) {
-                if(index==0){
+                /*if(index==0){
                     return false;
                 }else if(index == 3){
                     return false;
-                }
+                }*/
                 return true;
             }
                     
